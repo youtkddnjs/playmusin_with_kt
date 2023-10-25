5fdfd1f211381c8b7e0103d398676f8f9b18f4ce
mhha0 <119549975+mhha0@users.noreply.github.com>
2023-10-25 16:22:57 +0900

    create project

---
+++ app/src/main/java/mhha/sample/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package mhha.sample.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
